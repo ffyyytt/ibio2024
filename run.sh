118835a7dd41a050f53137261be153bf296f2f0a
Dương Anh Kiệt <43516744+ffyyytt@users.noreply.github.com>
2024-04-24 19:19:29 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # On peut éventuellement placer ici les commentaires SBATCH permettant de définir les paramètres par défaut de lancement :
 #SBATCH --gres gpu:2
 #SBATCH --time 1-23:50:00
-#SBATCH --cpus-per-task 8
+#SBATCH --cpus-per-task 18
 #SBATCH --mem-per-cpu 3G
 #SBATCH --mail-type FAIL,END
 
