a6716425cddc45605877d28bb1787b606b8dc077
Dương Anh Kiệt <43516744+ffyyytt@users.noreply.github.com>
2024-05-12 02:46:50 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 # L'argument '-l' est indispensable pour bénéficier des directives de votre .bashrc
  
 # On peut éventuellement placer ici les commentaires SBATCH permettant de définir les paramètres par défaut de lancement :
-#SBATCH --gres gpu:4
+#SBATCH --gres gpu:2
 #SBATCH --time 1-23:50:00
-#SBATCH --cpus-per-task 38
+#SBATCH --cpus-per-task 18
 #SBATCH --mem-per-cpu 3G
 #SBATCH --mail-type FAIL,END
 
